8b5dbf4ce96f3f99fa5cccaae009875a11553250
sevader14 <vsevolod.deriushkin@gmail.com>
2021-05-06 11:55:51 +0200

    adds get request to github (user hardcodeed), displays some info about user in div

--- src/components/GHSearch.jsx
+++ src/components/GHSearch.jsx
@@ -1,26 +1,44 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Button, Input } from "semantic-ui-react";
-import axios from 'axios'
+import axios from "axios";
 
 const GHSearch = () => {
-  const [searchQuery, setSearchQuery] = useState('')
-  const [searchResults, setSearchResults] = useState('')
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
 
-  const searchHandler = () => {
-    debugger
-  }
+
+  const searchHandler = async () => {
+    let response = await axios.get('https://api.github.com/search/users?q=sevader14')
+    setSearchResults(response.data.items)
+  };
+
+  let displayResults = searchResults.map((user) => {
+    return (
+      <>
+      <p>{user.login}</p>
+      <p>{user.id}</p>
+      <p>{user.html_url}</p>
+      </>
+    )    
+  })
 
   return (
     <>
       <Input
+      
         data-cy="user-search-input"
         type="text"
         name="search"
         placeholder="Input GH username"
       />
-      <Button onClick={() => searchHandler()} data-cy="search-button" name="search">
+      <Button
+        onClick={() => searchHandler()}
+        data-cy="search-button"
+        name="search"
+      >
         Search
       </Button>
+      <div data-cy="results-display">{displayResults}</div>
     </>
   );
 };
