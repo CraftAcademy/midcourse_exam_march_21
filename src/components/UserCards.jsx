05e753cbe8cd23967312f18322588f6e1753b5fe
kim_haaga <kim.haaga@live.se>
2021-05-06 11:50:32 +0200

    Add layout for user card

--- src/components/UserCards.jsx
+++ src/components/UserCards.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
-import {Card} from 'semantic-ui-react'
+import {Card, CardContent, Image, Button} from 'semantic-ui-react'
 
-const UserCards = () => {
+const UserCards = ({user: {login, avatar_url, html_url}}) => {
   return (
-    <Card>
-
+    <Card data-cy="user-card">
+      <Image data-cy='user-img' src={avatar_url}/>
+      <Card.Content>
+        <Card.Header data-cy="user-name" >
+          {login}
+        </Card.Header>
+        <CardContent>
+          <Button data-cy="user-btn" href={html_url} ></Button>
+        </CardContent>
+      </Card.Content>
     </Card>
   )
 }
