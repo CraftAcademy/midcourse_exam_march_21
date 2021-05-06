b296e4adc33c46c9bee984e95541a2e6c7309291
kim_haaga <kim.haaga@live.se>
2021-05-06 12:18:16 +0200

    fix typo

--- src/components/UserCards.jsx
+++ src/components/UserCards.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Card, CardContent, Image, Button} from 'semantic-ui-react'
 
-const UserCards = ({users: {login, avatar_url, html_url}}) => {
+const UserCards = ({user: {login, avatar_url, html_url}}) => {
   return (
     <Card data-cy="user-card">
       <Image data-cy='user-img' src={avatar_url}/>
