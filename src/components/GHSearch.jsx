412e61102d5506c9c643f75975923193f11b0157
lucas <laksteelhouse@gmail.com>
2021-05-07 14:36:26 +0200

    Adds controlled search functionality and onClick that fetches user results

--- src/components/GHSearch.jsx
+++ src/components/GHSearch.jsx
@@ -1,14 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Input } from 'semantic-ui-react'
+import axios from 'axios'
+import SearchResults from './SearchResults'
 
 const GHSearch = () => {
+  const [searchInput, setSearchInput] = useState()
+  const [seachResult, setSearchResult] = useState([])
+
+  const search = async () => {
+
+    let response = await axios.get(`https://api.github.com/search/users?q=${searchInput}`)
+    debugger
+  }
+
   return (
     <>
-      <Input type="text" name="search" placeholder="Input GH username"/>
-      <Button name="search">Search</Button>
+      <Input
+        type="text"
+        name="search"
+        data-cy='input-field'
+        placeholder="Input GH username"
+        onBlur={(event) => { setSearchInput(event.target.value) }} />
+      <Button
+        name="search"
+        data-cy='search-button'
+        onClick={search}
+      >
+        Search
+      </Button>
+      <SearchResults />
     </>
   )
 }
 
- // https://api.github.com/search/users?q=Lucas
+// https://api.github.com/search/users?q=Lucas
 export default GHSearch
---
+++ src/components/SearchResults.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SearchResults = ({searchResults}) => {
+  return (
+    <div style={styles.container}>
+      
+    </div>
+  )
+}
+
+export default SearchResults
+
+const styles = {
+  
+}
