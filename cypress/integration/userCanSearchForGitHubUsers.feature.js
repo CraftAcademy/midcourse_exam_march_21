5c40a3381abbb3af86e46165dbc0d91e5aae4a9d
Thomas <thomas@craftacademy.se>
2021-05-07 14:43:12 +0200

    adds axios as dependency refactors ui elements for testing with cypress adds functionality to call GH API for uses based on query soterd in state

--- cypress/integration/userCanSearchForGitHubUsers.feature.js
+++ cypress/integration/userCanSearchForGitHubUsers.feature.js
@@ -8,9 +8,13 @@ describe('User can search for Barack', () => {
       response: 'fx:gitHubSearchForBarack'
     })
     cy.visit('/')
+    cy.get('[data-cy=query-field]').type('Barack')
+    cy.get('[data-cy=query-button]').click()
   });
 
   it('is expected to display a list of users', () => {
-
+    cy.get('[data-cy=query-results]').within(()=> {
+      cy.get('[data-cy=user-10196880]').should('contain.text', 'PresidentObama')
+    })
   });
 });
--- src/components/GHSearch.jsx
+++ src/components/GHSearch.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Button, Input } from 'semantic-ui-react'
+import axios from "axios";
 
 const GHSearch = () => {
+  const [query, setQuery] = useState('')
+  const [users, setUsers] = useState([])
+
+  const fetchGHUsers = async () => {
+    const results = await axios.get(`https://api.github.com/search/users?q=${query}`)
+    setUsers(results.data.items)
+  }
+
+  const displayUsers = users.map(user => {
+    return (
+      <div key={user.id} data-cy={`user-${user.id}`}>{user.login}</div>
+    )
+  })
+
   return (
     <>
-      <Input type="text" name="search" placeholder="Input GH username"/>
-      <Button name="search">Search</Button>
+      <Input
+        data-cy="query-field"
+        type="text"
+        onChange={e => setQuery(e.target.value)}
+        placeholder="Input GH username" />
+      <Button
+        data-cy="query-button"
+        onClick={() => fetchGHUsers()}>
+        Search
+        </Button>
+      <div data-cy="query-results">
+        {displayUsers}
+      </div>
     </>
   )
 }
