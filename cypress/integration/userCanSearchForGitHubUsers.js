22b2111119aaed394168c50c0e83af4ddcc79252
lucas <laksteelhouse@gmail.com>
2021-05-07 14:20:42 +0200

    Writes tests for search functionality

---
+++ cypress/integration/userCanSearchForGitHubUsers.js
@@ -0,0 +1,23 @@
+describe('User can use the search field', () => {
+  describe('Successfully', () => {
+    beforeEach(() => {
+      cy.intercept('https://api.github.com/search/users?q=**', {fixture: 'searchFixture.json'})
+
+      cy.visit('/')
+      cy.get('[data-cy="input-field"]').type('Barack')
+      cy.get('[data-cy="search-button"]').click()
+    })
+  })
+
+  it('is expected to display a list of 5 users', () => {
+    cy.get('[data-cy="user-container"]').should('have.length', 5)
+  })
+
+  it('is expected to display the expected user information', () => {
+    cy.get('[data-cy="user-container"]').within(() => {
+      cy.get('[data-cy="username"]').should('contain', 'barack')
+      cy.get('[data-cy="avatar"]').should('have.attr', 'src', "https://avatars3.githubusercontent.com/u/681626?v=4")
+      cy.get('[data-cy="avatar"]').should('have.attr', 'href', "https://api.github.com/users/barack")
+    })
+  })
+})
